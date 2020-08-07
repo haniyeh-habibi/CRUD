fb0ccde1f90073a084559734cda9fc9fc960258c
haniyeh-habibi <haniye.habibi@gmail.com>
2020-08-07 19:42:16 +0430

    remove useless title

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -3,9 +3,7 @@
 @section('content')
     <div class="row">
         <div class="col-lg-12 margin-tb">
-            <div class="pull-left">
-                <h2>Laravel 7 CRUD Example from scratch - ItSolutionStuff.com</h2>
-            </div>
+            <div class="pull-left"></div>
             <div class="pull-right">
                 <a class="btn btn-success" href="{{ route('products.create') }}"> Create New Product</a>
             </div>
